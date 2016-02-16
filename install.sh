6e02e40eb1f56be27567f33485dab3813c4a1441
Nathan Phetteplace <nathan.phetteplace@rewardstyle.com>
2016-02-16 16:35:46 -0600

    Add neobundle submodule and include with install

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-# Terminal
+##############################################################################
+# Bash
+##############################################################################
 ln -sfv "`pwd`/bash/bash_profile" ~/.bash_profile
 ln -sfv "`pwd`/bash/inputrc" ~/.inputrc
 ln -sfv "`pwd`/bash/ackrc" ~/.ackrc
 
+##############################################################################
 # Vim
+##############################################################################
 ln -sfv "`pwd`/vim/vimrc" ~/.vimrc
 ln -sfv "`pwd`/vim/gvimrc" ~/.gvimrc
+
+if [ -d ~/.vim/bundle/neobundle.vim ]; then
+    if [ ! -L ~/.vim/bundle/neobundle.vim ]; then
+        mv -v ~/.vim/bundle/neobundle.vim ~/.vim/bundle/neobundle.vim.bak
+    fi
+fi
+
+ln -sfv "`pwd`/vim/neobundle.vim" ~/.vim/bundle/
