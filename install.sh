f90b584ac8745fd669b167638f8d5dc7d17d78eb
Nathan Phetteplace <nathan.phetteplace@rewardstyle.com>
2016-02-17 11:26:26 -0600

    Major improvements to shell files

--- install.sh
+++ install.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
-##############################################################################
-# Shell
-##############################################################################
-ln -sfv "`pwd`/shell/bash_profile" ~/.bash_profile
-ln -sfv "`pwd`/shell/inputrc" ~/.inputrc
-ln -sfv "`pwd`/shell/ackrc" ~/.ackrc
+# Get the absolute path to this file.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-##############################################################################
-# Vim
-##############################################################################
-ln -sfv "`pwd`/vim/vimrc" ~/.vimrc
-ln -sfv "`pwd`/vim/gvimrc" ~/.gvimrc
-
-if [ -d ~/.vim/bundle/neobundle.vim ]; then
-    if [ ! -L ~/.vim/bundle/neobundle.vim ]; then
-        mv -v ~/.vim/bundle/neobundle.vim ~/.vim/bundle/neobundle.vim.bak
+# Back up neobundle.vim if it was previously installed.
+# It will be replaced with a symlink.
+if [ -d $HOME/.vim/bundle/neobundle.vim ]; then
+    if [ ! -L $HOME/.vim/bundle/neobundle.vim ]; then
+        mv -v $HOME/.vim/bundle/neobundle.vim $HOME/.vim/bundle/neobundle.vim.bak
     fi
 fi
 
-ln -sfv "`pwd`/vim/neobundle.vim" ~/.vim/bundle/
+# Symlink necessary files and directories.
+ln -sfv $DIR/shell/bash_profile $HOME/.bash_profile
+ln -sfv $DIR/shell/inputrc      $HOME/.inputrc
+ln -sfv $DIR/shell/ackrc        $HOME/.ackrc
+ln -sfv $DIR/vim/vimrc          $HOME/.vimrc
+ln -sfv $DIR/vim/gvimrc         $HOME/.gvimrc
+ln -sfv $DIR/vim/neobundle.vim  $HOME/.vim/bundle/
