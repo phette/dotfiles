ca7e48b49136ed2c8d6071361ca1e714ff25689d
Nathan Phetteplace <nathan.phetteplace@rewardstyle.com>
2026-05-15 16:44:13 -0500

    Refactor Neovim configuration and streamline installation process for plugins

--- go.sh
+++ go.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# Install docker-credential-ecr-helper (so we don't have to log into ECR frequently).
-# Don't forget to set up ~/.docker/config.json.
-go get github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login
-
 # For unit testing.
 go get github.com/smartystreets/goconvey
